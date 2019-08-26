0eb09f5190806bd8234f2280ab7d667fa0160800
anna-postawka <anna.postawka@gmail.com>
2019-08-26 17:58:23 +0200

    added empty methods

--- src/js/View.js
+++ src/js/View.js
@@ -1,1 +1,18 @@
-export default class View {}
+export default class View {
+    constructor() {}
+
+    setCity(city) {}
+    setCountry(country) {}
+    setCurrentIcon(icon) {}
+    setCurrentDescription(description) {}
+    setCurrentTemperature(temperature) {}
+    setTemperatureUnit(unit) {}
+    setCurrentPressure(pressure) {}
+    setCurrentHumidity(humidity) {}
+    setCurrentWindSpeed(windSpeed) {}
+    setWindSpeedUnit(unit) {}
+    setCurrentWindDeg(windDeg) {}
+    setMinTemperatureFor5Days(temperature) {}
+    setMaxTemperatureFor5Days(temperature) {}
+
+}
