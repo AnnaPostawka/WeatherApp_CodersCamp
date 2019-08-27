da23e7f37f76c5863d9d9301af2610fb0af03646
Patryk Wydmuch <pvydmuch@gmail.com>
2019-08-27 23:51:49 +0200

    Add localStorage

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -1,26 +1,41 @@
 export default class Controller {
-    constructor(model, view) {
-      this.model = model
-      this.view = view
-      this.view.bindGetWeatherForCity(this.handleGetWeatherForCity);
+  constructor(model, view) {
+    this.model = model
+    this.view = view
+    this.view.bindGetWeatherForCity(this.handleGetWeatherForCity);
+    this._checkLocalStorage()
+  }
+
+  _checkLocalStorage() {
+    if (localStorage.getItem("cityName")) {
+      window.addEventListener('load', event => {
+        this.model.getWeatherForCity(localStorage.getItem("cityName"))
+          .then(weatherData => this.view.displayWeather(weatherData))
+      })
+    } else {
       this.view.bindGetWeatherForCoords(this.handleGetWeatherForCoords);
     }
-  
-    handleGetWeatherForCity = (cityName) => {
-      this.model.getWeatherForCity(cityName)
+  }
+  handleGetWeatherForCity = (cityName) => {
+    const elem = this.view.createElement("p")
+    const last = this.view.app.querySelector("p:last-of-type")
+    if (last !== null) {
+      this.view.app.removeChild(last)
+    }
+    this.model.getWeatherForCity(cityName)
       .then((weatherData) => {
         this.view.displayWeather(weatherData);
+        localStorage.setItem("cityName", cityName)
       })
-      .catch(err =>
-        console.log(err)
-      );
-    }
-  
-    handleGetWeatherForCoords = (lat, long) => {
-      this.model.getWeatherForCoords(lat, long)
+      .catch(err => {
+        elem.innerHTML = `Nie ma miasta o nazwie ${cityName}`;
+        this.view.app.appendChild(elem)
+      });
+  }
+  handleGetWeatherForCoords = (lat, long) => {
+    this.model.getWeatherForCoords(lat, long)
       .then((weatherData) => {
         this.view.displayWeather(weatherData);
       });
-    }
   }
-
+}
