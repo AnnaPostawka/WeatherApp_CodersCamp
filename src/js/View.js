40689f63f400c5935b922ff3630256f482387d86
anna-postawka <anna.postawka@gmail.com>
2019-08-28 16:24:02 +0200

    View class in progress

--- src/js/View.js
+++ src/js/View.js
@@ -1,16 +1,44 @@
 export default class View {
-    constructor() {}
+    constructor() {
+        this.setCurrentWindSpeed(14, 'mph');
+    }
 
-    setDateAndTime(date) {}
-    setCityName(city) {}
+    setDateAndTime(date) {
+        document.querySelector('.currentInfo__date').innerHTML = date;
+    }
+
+    /*
+    setCityName(city) {
+        document.querySelector('.currentInfo__city').innerHTML = city;
+    }
     setCountry(country) {}
+    */
+    setCityAndCountry(city, country) {
+        document.querySelector('.currentInfo__city').innerHTML = `${city}, ${country}`;
+    }
+
     setCurrentIcon(icon) {}
-    setCurrentDescription(description) {}
+    setCurrentDescription(description) {
+        document.querySelector('.currentInfo__sky').innerHTML = description;
+    }
     setCurrentTemperature(temperature) {}
     setTemperatureUnit(unit) {}
-    setCurrentHumidity(humidity) {}
-    setCurrentWindSpeed(windSpeed) {}
+    setCurrentHumidity(humidity) {
+        document.querySelector('.currentInfo__humidity').getElementsByTagName('span')[0].innerHTML = `${humidity}%`;
+    }
+
+    /*
+    setCurrentWindSpeed(windSpeed) {
+
+        document.querySelector('.currentInfo__wind').getElementsByTagName('span')[0].innerHTML = `${windSpeed}`;
+    }
     setWindSpeedUnit(unit) {}
+    */
+    setCurrentWindSpeed(windSpeed, unit) {
+
+        document.querySelector('.currentInfo__wind').getElementsByTagName('span')[0].innerHTML = `${windSpeed} ${unit}`;
+    }
+
     setCurrentWindDeg(windDeg) {}
     set5DaysMinTemperature(temperature) {}
     set5DaysMaxTemperature(temperature) {}
