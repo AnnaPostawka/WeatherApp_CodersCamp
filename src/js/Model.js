e60eaf39ba8a90b9edb92cb46195992d7c70b7d4
Izabela Popiel <izabelapopiel1@gmail.com>
2019-08-30 13:50:06 +0200

    method for getting 5 days forecast added

--- src/js/Model.js
+++ src/js/Model.js
@@ -4,6 +4,7 @@ export default class Model {
         this._view = view;
         this._apiKey = '2af7e2e12429ce3e4a759ae7a80c24f1';
         this._weatherData = undefined;
+        this._forecastData = undefined;
         this._units = 'metric'; //metric or imperial or deafult
     }
 
@@ -12,7 +13,8 @@ export default class Model {
     changedLocation(location) {
         this._getWeatherData(location)
             .then((data) => {
-                this._weatherData = data;
+                this._weatherData = data[0];
+                this._forecastData = data[1];
                 console.log(data);
             });
     }
@@ -25,7 +27,8 @@ export default class Model {
         if (this._weatherData) {
             this._getWeatherData([this._weatherData.name])
                 .then((data) => {
-                    this._weatherData = data;
+                    this._weatherData = data[0];
+                    this._forecastData = data[1];
                     console.log(data);
                 });
         }
@@ -33,38 +36,42 @@ export default class Model {
 
     //location is an array with city name ['wroclaw']
     //or coords ['lat', 'long'] 
-    _getWeatherData(location) {
+    async _getWeatherData(location) {
         let request;
+        let requestForecast;
         if (location.length == 1) {
             const city = location[0];
             request = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${this._units}&APPID=${this._apiKey}`;
+            requestForecast = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${this._units}&APPID=${this._apiKey}`;
         } else {
             const lat = location[0];
             const long = location[1];
             request = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${this._units}&APPID=${this._apiKey}`;
+            requestForecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=${this._units}&APPID=${this._apiKey}`;
         }
 
-        const weatherData = this._weatherAPIRequest(request)
-            .then(data => {
-                let tempUnit;
-                let windSpeedUnit;
+        let tempUnit;
+        let windSpeedUnit;
 
-                switch (this._units) {
-                    case 'default':
-                        tempUnit = 'K';
-                        windSpeedUnit = 'm/s';
-                        break;
-                    case 'metric':
-                        tempUnit = String.fromCharCode(176) + 'C';
-                        windSpeedUnit = 'm/s';
-                        break;
-                    case 'imperial':
-                        tempUnit = String.fromCharCode(176) + 'F';
-                        windSpeedUnit = 'mph';
-                        break;
-                    default:
-                        console.log('wrong unit')
-                }
+        switch (this._units) {
+            case 'default':
+                tempUnit = 'K';
+                windSpeedUnit = 'm/s';
+                break;
+            case 'metric':
+                tempUnit = String.fromCharCode(176) + 'C';
+                windSpeedUnit = 'm/s';
+                break;
+            case 'imperial':
+                tempUnit = String.fromCharCode(176) + 'F';
+                windSpeedUnit = 'mph';
+                break;
+            default:
+                console.log('wrong unit')
+        }
+
+        const weatherData = await this._weatherAPIRequest(request)
+            .then(data => {
 
                 this._view.setDateAndTime(this._getlocalTime(data.dt, data.timezone));
                 this._view.setCityAndCountry(data.name, data.sys.country);
@@ -78,7 +85,16 @@ export default class Model {
 
                 return data;
             });
-        return weatherData;
+
+        const forecastData = await this._weatherAPIRequest(requestForecast)
+            .then((forecast) => {
+                return this._forecastMinMaxTemps(forecast);
+            }).then(minMaxTemps => {
+                this._view.set5DaysTemperature(minMaxTemps, tempUnit);
+                return minMaxTemps
+            });
+
+        return [weatherData, forecastData];
     }
 
     //method fetching weather data
@@ -95,5 +111,58 @@ export default class Model {
         now.setMinutes(now.getMinutes() + timezone / 60);
 
         return now;
+    }
+
+    _forecastMinMaxTemps(forecast) {
+        const temp5Days = [];
+        const timezoneOffset = forecast.city.timezone;
+        const startingDate = this._getlocalTime(forecast.list[0].dt, timezoneOffset).setHours(0, 0, 0, 0);
+        const dates = [];
+        const allTemps = [[], [], [], [], [], []];
+
+        forecast.list.forEach(data => {
+            const localDate = this._getlocalTime(data.dt, timezoneOffset).setHours(0, 0, 0, 0);
+            const localDay = this._getlocalTime(data.dt, timezoneOffset).getDate();
+            const localMonth = this._getlocalTime(data.dt, timezoneOffset).getMonth() + 1;
+            let day;
+            let month;
+
+            localDay.toString().length == 1 ? day = '0' + localDay : day = localDay;
+            localMonth.toString().length == 1 ? month = '0' + localMonth : month = localMonth;
+            const date = `${day}.${month}`;
+
+            if (!dates.includes(date)) dates.push(date);
+
+            switch (localDate) {
+                case startingDate:
+                    allTemps[0].push(data.main.temp);
+                    break;
+                case new Date(startingDate).setDate(new Date(startingDate).getDate() + 1):
+                    allTemps[1].push(data.main.temp);
+                    break;
+                case new Date(startingDate).setDate(new Date(startingDate).getDate() + 2):
+                    allTemps[2].push(data.main.temp);
+                    break;
+                case new Date(startingDate).setDate(new Date(startingDate).getDate() + 3):
+                    allTemps[3].push(data.main.temp);
+                    break;
+                case new Date(startingDate).setDate(new Date(startingDate).getDate() + 4):
+                    allTemps[4].push(data.main.temp);
+                    break;
+                case new Date(startingDate).setDate(new Date(startingDate).getDate() + 5):
+                    allTemps[5].push(data.main.temp);
+                    break;
+            }
+        });
+
+        for (let i = 1; i < dates.length; i++) {
+            temp5Days.push({
+                date: dates[i],
+                temp_min: Math.min(...allTemps[i]),
+                temp_max: Math.max(...allTemps[i]),
+            });
+        }
+        console.log(temp5Days);
+        return temp5Days;
     }
 }
