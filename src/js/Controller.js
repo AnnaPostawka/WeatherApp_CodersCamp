bf1b8f700c2f0e0647b7088c66834ae3cc77f073
Patryk Wydmuch <pvydmuch@gmail.com>
2019-09-01 23:02:17 +0200

    Adjust to other classes

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -1,41 +1,68 @@
+const Units = {
+  METRIC: 'metric',
+  IMPERIAL: 'imperial'
+}
+
 export default class Controller {
   constructor(model, view) {
     this.model = model
     this.view = view
-    this.view.bindGetWeatherForCity(this.handleGetWeatherForCity);
     this._checkLocalStorage()
+    this._onLocationChange()
+    this._onUnitsChanged()
+
+  }
+  _onLocationChange() {
+    const form = document.querySelector('.form')
+    form.addEventListener('submit', event => {
+      event.preventDefault()
+      const input = form.querySelector('.form__input');
+      this.model.changedLocation([input.value])
+      localStorage.setItem("cityName", [input.value])
+    })
+  }
+
+  _onUnitsChanged() {
+    const toggle = document.querySelector('#switch')
+    toggle.addEventListener('click', event => {
+      if (toggle.checked) {
+        this.model.changedUnits(Units.METRIC)
+        localStorage.setItem('units', Units.METRIC)
+      } else {
+        this.model.changedUnits(Units.IMPERIAL)
+        localStorage.setItem('units', Units.IMPERIAL)
+      }
+    })
   }
 
   _checkLocalStorage() {
-    if (localStorage.getItem("cityName")) {
+    if (localStorage.getItem('cityName')) {
       window.addEventListener('load', event => {
-        this.model.getWeatherForCity(localStorage.getItem("cityName"))
-          .then(weatherData => this.view.displayWeather(weatherData))
+        this.model.changedLocation([localStorage.getItem("cityName")])
       })
     } else {
-      this.view.bindGetWeatherForCoords(this.handleGetWeatherForCoords);
+      this._checkGeoLocation()
+    }
+    const units = localStorage.getItem('units')
+    if (units == Units.METRIC) {
+      document.querySelector('#switch').checked = true
+      this.model.changedUnits(Units.METRIC)
+    }
+    if (units == Units.IMPERIAL) {
+      document.querySelector('#switch').checked = false
+      this.model.changedUnits(Units.IMPERIAL)
     }
   }
-  handleGetWeatherForCity = (cityName) => {
-    const elem = this.view.createElement("p")
-    const last = this.view.app.querySelector("p:last-of-type")
-    if (last !== null) {
-      this.view.app.removeChild(last)
-    }
-    this.model.getWeatherForCity(cityName)
-      .then((weatherData) => {
-        this.view.displayWeather(weatherData);
-        localStorage.setItem("cityName", cityName)
-      })
-      .catch(err => {
-        elem.innerHTML = `Nie ma miasta o nazwie ${cityName}`;
-        this.view.app.appendChild(elem)
-      });
-  }
-  handleGetWeatherForCoords = (lat, long) => {
-    this.model.getWeatherForCoords(lat, long)
-      .then((weatherData) => {
-        this.view.displayWeather(weatherData);
-      });
+  _checkGeoLocation() {
+    window.addEventListener('load', event => {
+      if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(pos => {
+          const long = pos.coords.longitude;
+          const lat = pos.coords.latitude;
+          this.model.changedLocation([lat, long]);
+        })
+      }
+    })
   }
 }
