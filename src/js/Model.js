00e1978262c43e77610c71c786c25f5e1ac7abfa
Izabela Popiel <izabelapopiel1@gmail.com>
2019-09-02 15:25:58 +0200

    async methods refactorized

--- src/js/Model.js
+++ src/js/Model.js
@@ -10,91 +10,82 @@ export default class Model {
 
     //location is an array with city name ['wroclaw']
     //or coords ['lat', 'long'] 
-    changedLocation(location) {
-        this._getWeatherData(location)
-            .then((data) => {
-                this._weatherData = data[0];
-                this._forecastData = data[1];
-                console.log(data);
-            });
+    async changedLocation(location) {
+        const data = await this._getWeatherData(location)
+        this._weatherData = data[0];
+        this._forecastData = data[1];
     }
 
     //unit is set to new unit
     //if city has been already saved in weatherData
     //metod for getting weatherData is called for this city
-    changedUnits(unit) {
+    async changedUnits(unit) {
         this._units = unit;
         if (this._weatherData) {
-            this._getWeatherData([this._weatherData.name])
-                .then((data) => {
-                    this._weatherData = data[0];
-                    this._forecastData = data[1];
-                    console.log(data);
-                });
+            const data = await this._getWeatherData([this._weatherData.name]);
+            this._weatherData = data[0];
+            this._forecastData = data[1];
         }
     }
 
     //location is an array with city name ['wroclaw']
     //or coords ['lat', 'long'] 
     async _getWeatherData(location) {
-        let request;
-        let requestForecast;
-        if (location.length == 1) {
-            const city = location[0];
-            request = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${this._units}&APPID=${this._apiKey}`;
-            requestForecast = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${this._units}&APPID=${this._apiKey}`;
-        } else {
-            const lat = location[0];
-            const long = location[1];
-            request = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${this._units}&APPID=${this._apiKey}`;
-            requestForecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=${this._units}&APPID=${this._apiKey}`;
-        }
+        return new Promise(async (resolve) => {
+            
+            let request;
+            let requestForecast;
+            if (location.length == 1) {
+                const city = location[0];
+                request = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${this._units}&APPID=${this._apiKey}`;
+                requestForecast = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${this._units}&APPID=${this._apiKey}`;
+            } else {
+                const lat = location[0];
+                const long = location[1];
+                request = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${this._units}&APPID=${this._apiKey}`;
+                requestForecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=${this._units}&APPID=${this._apiKey}`;
+            }
 
-        let tempUnit;
-        let windSpeedUnit;
+            let tempUnit;
+            let windSpeedUnit;
 
-        switch (this._units) {
-            case 'default':
-                tempUnit = 'K';
-                windSpeedUnit = 'm/s';
-                break;
-            case 'metric':
-                tempUnit = String.fromCharCode(176) + 'C';
-                windSpeedUnit = 'm/s';
-                break;
-            case 'imperial':
-                tempUnit = String.fromCharCode(176) + 'F';
-                windSpeedUnit = 'mph';
-                break;
-            default:
-                console.log('wrong unit')
-        }
+            switch (this._units) {
+                case 'default':
+                    tempUnit = 'K';
+                    windSpeedUnit = 'm/s';
+                    break;
+                case 'metric':
+                    tempUnit = String.fromCharCode(176) + 'C';
+                    windSpeedUnit = 'm/s';
+                    break;
+                case 'imperial':
+                    tempUnit = String.fromCharCode(176) + 'F';
+                    windSpeedUnit = 'mph';
+                    break;
+                default:
+                    console.log('wrong unit')
+            }
 
-        const weatherData = await this._weatherAPIRequest(request)
-            .then(data => {
+            const weatherData = await this._weatherAPIRequest(request);
 
-                this._view.setDateAndTime(this._getlocalTime(data.dt, data.timezone));
-                this._view.setCityAndCountry(data.name, data.sys.country);
-                this._view.setCurrentIcon(data.weather[0].id, this._dayOrNight(data.dt, data.timezone, data.sys.sunrise, data.sys.sunset));
-                this._view.setCurrentDescription(data.weather[0].description);
-                this._view.setCurrentTemperature(data.main.temp, tempUnit);
-                this._view.setCurrentHumidity(data.main.humidity, '%');
-                this._view.setCurrentWindSpeed(data.wind.speed, windSpeedUnit);
-                this._view.setCurrentWindDeg(data.wind.deg, String.fromCharCode(176));
-                this._view.setCurrentPressure(data.main.pressure, 'hPa');
+            this._view.setDateAndTime(this._getlocalTime(weatherData.dt, weatherData.timezone));
+            this._view.setCityAndCountry(weatherData.name, weatherData.sys.country);
+            this._view.setCurrentIcon(weatherData.weather[0].id, this._dayOrNight(weatherData.dt, weatherData.timezone, weatherData.sys.sunrise, weatherData.sys.sunset));
+            this._view.setCurrentDescription(weatherData.weather[0].description);
+            this._view.setCurrentTemperature(weatherData.main.temp, tempUnit);
+            this._view.setCurrentHumidity(weatherData.main.humidity, '%');
+            this._view.setCurrentWindSpeed(weatherData.wind.speed, windSpeedUnit);
+            this._view.setCurrentWindDeg(weatherData.wind.deg, String.fromCharCode(176));
+            this._view.setCurrentPressure(weatherData.main.pressure, 'hPa');
 
-                return data;
-            });
+            const forecastData = await this._weatherAPIRequest(requestForecast);
+            const minMaxTemps = this._forecastMinMaxTemps(forecastData);
+            this._view.set4DaysTemperature(minMaxTemps, tempUnit);
 
-        const forecastData = await this._weatherAPIRequest(requestForecast)
-            .then((forecast) => {
-                return this._forecastMinMaxTemps(forecast);
-            }).then(minMaxTemps => {
-                this._view.set4DaysTemperature(minMaxTemps, tempUnit);
-                return minMaxTemps
-            });
+            console.log([weatherData, minMaxTemps]);
+            resolve([weatherData, minMaxTemps]);
+        })
 
-        return [weatherData, forecastData];
     }
 
     //method fetching weather data
