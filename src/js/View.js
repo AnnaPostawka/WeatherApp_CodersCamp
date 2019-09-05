a99c07a6d3a3a0bf904222f9110329a01f80aeee
anna-postawka <anna.postawka@gmail.com>
2019-09-05 11:01:37 +0200

    method resetFields in progress

--- src/js/View.js
+++ src/js/View.js
@@ -36,41 +36,75 @@ export default class View {
 
 
 
+    _setInnerHTML(className, innerHTML) {
+        document.querySelector(className).innerHTML = innerHTML;
+    }
+
     setDateAndTime(date) {
         document.querySelector('.currentInfo__date').innerHTML = date;
     }
+
     setCityAndCountry(city, country) {
-        document.querySelector('.currentInfo__city').innerHTML = `${city}, ${country}`;
+        this._setCityAndCountry(`${city}, ${country}`);
     }
+    _setCityAndCountry(innerHTML) {
+        this._setInnerHTML('.currentInfo__city', innerHTML);
+    }
+
     setCurrentIcon(id, dayOrNight) {
         var elem = document.querySelector('.temperatureBox__weatherIcon');
         elem.classList.add('wi');
         elem.classList.add(`wi-owm-${dayOrNight}-${id}`);
     }
+
     setCurrentDescription(description) {
         document.querySelector('.currentInfo__sky').innerHTML = description;
     }
-    setCurrentTemperature(temperature, unit) {
-        document.querySelector('.currentTemp').innerHTML = `${temperature}${unit}`;
-    }
-    setCurrentWindSpeed(windSpeed, unit) {
 
-        document.querySelector('.wind-value').innerHTML = `${windSpeed} ${unit}`;
+    setCurrentTemperature(temperature, unit) {
+        this._setCurrentTemperature(`${temperature}${unit}`);
     }
+    _setCurrentTemperature(innerHTML) {
+        this._setInnerHTML('.currentTemp', innerHTML)
+    }
+
+    setCurrentWindSpeed(windSpeed, unit) {
+        this._setCurrentWindSpeed("Wind: ", `${windSpeed} ${unit}`);
+    }
+    _setCurrentWindSpeed(label, innerHTML) {
+        this._setInnerHTML('.currentInfo__wind', label);
+        this._setInnerHTML('.wind-value', innerHTML);
+    }
+
     setCurrentWindDeg(windDeg) {
         var elem = document.querySelector('.currentInfo__windIcon');
         elem.classList.add('wi');
         elem.classList.add('wi-wind');
         elem.classList.add(`from-${windDeg}-deg`);
     }
-    setCurrentHumidity(humidity, unit) {
-        document.querySelector('.humidity-value').innerHTML = `${humidity}${unit}`;
-    }
-    setCurrentPressure(pressure, unit) {
-        document.querySelector('.pressure-value').innerHTML = `${pressure} ${unit}`
-    }
-    set5DaysTemperature(temp, unit) {
 
+    setCurrentHumidity(humidity, unit) {
+        this._setCurrentHumidity("Humidity: ", `${humidity}${unit}`);
+    }
+    _setCurrentHumidity(label, innerHTML) {
+        this._setInnerHTML('.currentInfo__humidity', label);
+        this._setInnerHTML('.humidity-value', innerHTML);
+    }
+
+    setCurrentPressure(pressure, unit) {
+        this._setCurrentPressure("Pressure: ", `${pressure} ${unit}`);
+    }
+    _setCurrentPressure(label, innerHTML) {
+        this._setInnerHTML('.currentInfo__pressure', label);
+        this._setInnerHTML('.pressure-value', innerHTML);
+    }
+
+    set5DaysTemperature(temp, unit) {
+        const labels = document.querySelector('.forecast__day');
+        const date = labels.firstElementChild;
+        date.innerHTML = "Date";
+        date.nextElementSibling.innerHTML = "Min";
+        labels.lastElementChild.innerHTML = "Max";
         for (let i = 0; i < 4; i++) {
             document.querySelector('.forecast').querySelectorAll('.day')[i].innerHTML = temp[i].day;
             document.querySelector('.forecast').querySelectorAll('.min')[i].innerHTML = temp[i].temp_min + unit;
@@ -80,11 +114,26 @@ export default class View {
 
 
     setInputValidationMessage(isValid) {
-        var cityInput = document.querySelector('.form__input');
+        const cityInput = document.querySelector('.form__input');
         if (isValid) {
             cityInput.setCustomValidity("");
         } else {
             cityInput.setCustomValidity("We cannot find what you're looking for, sorry! Try other city.");
         }
     }
+
+    resetFields() {
+        this.setDateAndTime("");
+        this._setCityAndCountry("");
+        this.setCurrentDescription("");
+        this._setCurrentTemperature("");
+        this._setCurrentWindSpeed("", "");
+        this._setCurrentHumidity("", "");
+        this._setCurrentPressure("", "");
+
+        /* Brakuje setCurrentIcon, setCurrentWindDeg, set5DaysTemperature */
+
+    }
+
+
 }
